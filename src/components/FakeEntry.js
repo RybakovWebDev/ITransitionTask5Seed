241ec328f1e6bbf41f5846ef61062de2c13226d1
AR <rybakovwebdev@gmail.com>
2023-01-27 17:43:14 +0400

    Replaced cell view with a table

--- src/components/FakeEntry.js
+++ src/components/FakeEntry.js
@@ -2,41 +2,40 @@ export const FakeEntry = (props) => {
   const renderEntry = () => {
     return props.fakeEntries.map((el, i) => {
       return (
-        <div className='entry-cont' key={props.uuidv4()}>
-          <div className='entry-info-cont'>
-            <div className='entry-info-fields-cont'>
-              <h4 className='entry-info-field'>
-                Index: <br />
-                ID: <br />
-                Full Name: <br />
-                Phone number: <br />
-                Address:
-              </h4>
-            </div>
-            <div className='entry-info-values-cont'>
-              <h4 className='entry-info-value'>
-                {i + 1}
-                <br />
-                {el.id}
-                <br />
-                {el.fullName}
-                <br />
-                {el.phone}
-                <br />
-                {el.address}
-              </h4>
-            </div>
-          </div>
-
-          {i !== props.fakeEntries.length - 1 ? <div className='horizontal-separator'></div> : ""}
-        </div>
+        <tr>
+          <th scope='row'>{i + 1}</th>
+          <td>{el.id}</td>
+          <td>{el.fullName}</td>
+          <td>{el.address}</td>
+          <td>{el.phone}</td>
+        </tr>
       );
     });
   };
 
   return (
     <article className='entries-parent' onScroll={props.scrollHandler}>
-      {renderEntry()}
+      <table>
+        <tr>
+          <th className='table-index' scope='col'>
+            Index
+          </th>
+          <th className='table-id' scope='col'>
+            ID
+          </th>
+          <th className='table-name' scope='col'>
+            Full name
+          </th>
+          <th className='table-address' scope='col'>
+            Address
+          </th>
+          <th className='table-phone' scope='col'>
+            Phone number
+          </th>
+        </tr>
+
+        {renderEntry()}
+      </table>
     </article>
   );
 };
